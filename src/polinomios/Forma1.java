9c49404d121523b7af43eed9581306168e808a97
andre <andre@Romdres>
2026-03-01 21:39:48 -0500

    Actualizacion de andres en polinomios java Actualización a la interfaz gráfica, estructura mejorada + Codigo de jota en inf + Forma 1

--- src/polinomios/Forma1.java
+++ src/polinomios/Forma1.java
@@ -1,21 +1,22 @@
 package polinomios;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Samuel Sosa - Juan Jose Restrepo - Santiago Ayala
- * //8x^2-3x^5+2x-10
+ * @author Samuel Sosa - Juan Jose Restrepo - Santiago Ayala //8x^2-3x^5+2x-10
  */
 public class Forma1 {
-    //atributos
-    private int Du,VPF1[];
-    //metodos
-    
-    //constructor
 
+    //atributos
+    private int Du, VPF1[];
+    //metodos
+
+    //constructor
     public Forma1(int Grado) {
-        Du = Grado+1;
-        VPF1 = new int[Du+1];
-        VPF1[0]= Grado;
+        Du = Grado + 1;
+        VPF1 = new int[Du + 1];
+        VPF1[0] = Grado;
     }
 
     public int getDu() {
@@ -25,12 +26,15 @@ public class Forma1 {
     public void setDu(int du) {
         this.Du = du;
     }
+
     public int[] getVPF1() {
         return VPF1;
     }
- public void setVPF1(int[] VPF1) {
+
+    public void setVPF1(int[] VPF1) {
         this.VPF1 = VPF1;
     }
+
     public int getVPF1(int i) {
         return VPF1[i];
     }
@@ -38,77 +42,122 @@ public class Forma1 {
     public void setVPF1(int d, int i) {
         this.VPF1[i] = d;
     }
-    
 
     //Insertar el polinomio
-    
-  
     //1. Insertar termino
     //2. Eliminar termino
     //3. Mostrar
-    //4. Reconstruir
+    //4. Reconstruir - ajustar
     //5. Evaluar
     //6. Sumar
     //7. Multiplicar
-    
     //Llenar el polinomio
-    public void LlenarPoli(String Vs[]){
-         int j=0, coe=0, mayorExp=0, exp=0;
-        
-         for(int i=1; i<Vs.length; i+=2){
-         exp = Integer.parseInt(Vs[i]);
-         if(exp > mayorExp){
-             mayorExp = exp;
-         }
-         VPF1 = new int [mayorExp + 1];
+    public void LlenarPoli(String Vs[]) {
+        int coe = 0, mayorExp = 0, exp = 0;
+
+        for (int i = 1; i < Vs.length && Vs[i] != null; i += 2) {
+            exp = Integer.parseInt(Vs[i]);
+            if (exp > mayorExp) {
+                mayorExp = exp;
+            }
         }
-        
-         for(int i=0;  i<Vs.length ; i+=2){
-             coe= Integer.parseInt(Vs[i]);
-             exp= Integer.parseInt(Vs[i+1]);
-             VPF1[exp] = coe;
-         }
-    }
-    
-   
 
-    public void insertar (int c, int e){
-     
-        for (int i=2; i<=Du; i+=2){
-            
-        if(VPF1[i]==e){
-       VPF1[i-1]+=c;
-       
-       
-        } 
-        if(VPF1[i]>e){
-        int []Va=new int [Du+3]; 
-        for(int j=2;j<i;j+=2){
-          Va[j-1]=c;
-          Va[j]=e;
-         Va[Du+1]= VPF1[i-1];
-         Va[Du+2]=VPF1[i];
-         
+        VPF1 = new int[mayorExp + 1];
+        VPF1[0] = mayorExp;
+
+        for (int i = 0; i < Vs.length && Vs[i] != null; i += 2) {
+            coe = Integer.parseInt(Vs[i - 1]);
+            // exp = Integer.parseInt(Vs[i]);     
+
+            VPF1[i] = coe;
         }
-               }
-      //  if(VPF1[i]<e){
-     //   int []Va=new int [Du+3];
-      //  for (int j=2;j<=i;j+=2);
-        
-      //  }
-             }
- 
+        /*
+    for (int i = 0; i < VPF1.length; i++) {
+        JOptionPane.showMessageDialog(null, 
+            "Exponente: " + i + "  Coeficiente: " + VPF1[i]);
+        JOptionPane.showMessageDialog(null, 
+            "Mayor Exponente: " + mayorExp ) ;
+    } /*/
     }
 
+    public void insertar(int c, int e) {
 
-    
-    
-    public void eliminar(){
-        
+        for (int i = 2; i <= Du; i += 2) {
+
+            if (VPF1[i] == e) {
+                VPF1[i - 1] += c;
+
+            }
+            if (VPF1[i] > e) {
+                int[] Va = new int[Du + 3];
+                for (int j = 2; j < i; j += 2) {
+                    Va[j - 1] = c;
+                    Va[j] = e;
+                    Va[Du + 1] = VPF1[i - 1];
+                    Va[Du + 2] = VPF1[i];
+
+                }
+            }
+            //  if(VPF1[i]<e){
+            //   int []Va=new int [Du+3];
+            //  for (int j=2;j<=i;j+=2);
+
+            //  }
+        }
+
     }
 
-    public void Sumar(){
-        
+    public void MostrarPolinomio() {
+
+        String poli = "";
+
+        for (int i = VPF1.length - 1; i >= 0; i--) {
+
+            int coe = VPF1[i];
+
+            if (coe != 0) {
+
+                // Signo
+                if (coe > 0 && !poli.equals("")) {
+                    poli += "+";
+                }
+
+                // Casos especiales
+                if (i == 0) { // término independiente
+                    poli += coe;
+                } else if (i == 1) { // exponente 1
+                    if (coe == 1) {
+                        poli += "x";
+                    } else if (coe == -1) {
+                        poli += "-x";
+                    } else {
+                        poli += coe + "x";
+                    }
+                } else { // exponentes mayores
+                    if (coe == 1) {
+                        poli += "x^" + i;
+                    } else if (coe == -1) {
+                        poli += "-x^" + i;
+                    } else {
+                        poli += coe + "x^" + i;
+                    }
+                }
+            }
+        }
+
+        if (poli.equals("")) {
+            poli = "0";
+        }
+
+        JOptionPane.showMessageDialog(null, "Polinomio: " + poli);
     }
-    
+
+    public void eliminar() {
+
+    }
+
+    public void Sumar() {
+
+    }
+
 }
