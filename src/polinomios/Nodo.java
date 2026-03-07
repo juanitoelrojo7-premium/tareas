3f536084e5666ed86efa2daabb285d7cadf00472
andre <andre@Romdres>
2026-03-06 19:24:06 -0500

    Actualizacion de andres en Forma1 java Forma 1, primer metodo polli....

--- src/polinomios/Nodo.java
+++ src/polinomios/Nodo.java
@@ -1,12 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package polinomios;
 
 /**
  *
- * @author Juan jose, andres barrera
+ * @author Juan Jose Restrepo, Santiago Andres Barrera
  * 
  */
 public class Nodo {
